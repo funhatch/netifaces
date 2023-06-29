8f4256b87cff990d8318e0d81966d59412b967ff
Graeme Love <me@graeme.love>
2023-06-29 14:24:34 -0400

    Updated setup.py Python version info

--- setup.py
+++ setup.py
@@ -588,13 +588,8 @@ setup (name='netifaces',
     'License :: OSI Approved :: MIT License',
     'Topic :: System :: Networking',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11'
     ],
        ext_modules=[iface_mod])
